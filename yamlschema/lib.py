cfab167acda6a650c3c27615e820e45b5611872a
Ashley Fisher <fish.ash@gmail.com>
2017-10-27 15:31:21 -0700

    generalized the tool and made tests pass

--- yamlschema/lib.py
+++ yamlschema/lib.py
@@ -1,4 +1,6 @@
-
+"""
+Validate a YAML config file
+"""
 
 import os
 import sys
@@ -12,41 +14,37 @@ from codado.tx import Main
 
 class ValidateConfig(Main):
     """
-    Validate that config.yml is valid based on config.schema.yml
+    Validate config.yml based on config.schema.yml
     """
-    synopsis = "path/to/config.yml | --schema path/to/schema.yml"
+    synopsis = "path/to/config.yml path/to/schema.yml"
     optFlags = [
         ['schema', None, 'Display normalized schema from file']
     ]
     optParameters = []
 
-    def parseArgs(self, configFile=DEFAULT_CONFIG):
+    def parseArgs(self, configFile, configSchema):
         if not os.access(configFile, os.R_OK):
-            raise OSError("Cannot read config or schema file %s" % configFile)
+            raise OSError("Cannot read config file %s" % configFile)
+        if not os.access(configSchema, os.R_OK):
+            raise OSError("Cannot read schema file %s" % configSchema)
+
         self['configFile'] = configFile
+        self['configSchema'] = configSchema
 
     def postOptions(self):
         """
         Validate config file
         """
-        if self['schema']:
-            print >>sys.stderr, "Output {}\n".format(self['configFile'])
-            with open(self['configFile']) as f:
-                res = yaml.dump(yaml.load(f))
-                print res
-                return res
-        else:
-            print >>sys.stderr, "Validating {}\n".format(self['configFile'])
-            res = validateConfig(self['configFile'])
-            print >>sys.stderr, "{} is valid\n".format(self['configFile'])
-            return res
+        print >>sys.stderr, "Validating {}\n".format(self['configFile'])
+        res = validateConfig(self['configFile'], self['configSchema'])
+        print >>sys.stderr, "{} is valid\n".format(self['configFile'])
+        return res
 
 
 def validateConfig(configYML, configSchemaYML):
     """
-    Validate that values in a config file are valid
+    Validate configYML based off of configSchemaYML
     """
-
     config = yaml.load(open(configYML))
     schema = yaml.load(open(configSchemaYML))
     try:
--- yamlschema/test/test_lib.py
+++ yamlschema/test/test_lib.py
@@ -1,78 +1,74 @@
 """
-Utility to import sample data and staff batch data, should import data
+Tests for YAML config file validation
 """
+
 import sys
 import os
 
 from jsonschema import ValidationError
 
-from pytest import fixture
+from codado import fromdir
 
-from mock import patch, Mock
+from pytest import fixture, raises
 
-from yamlschema.lib import validateconfig
+from mock import patch
+
+from yamlschema.lib import ValidateConfig
 
 
 @fixture
 def options():
-    opt = validateconfig.ValidateConfig()
-    opt['configFile'] = aorta.fromaortaPackage('test/test_cli/test_config.yml')
-    opt['schema'] = False
+    options = ValidateConfig()
+    options['configSchema'] = fromdir(__file__)('test_config.schema.yml')
+    return options
 
 
 @fixture
 def configGood():
-    return aorta.fromaortaPackage('test/test_cli/test_config.schema.yml')
+    return fromdir(__file__)('test_config.yml')
     
 
 @fixture
 def configBad():
-    return aorta.fromaortaPackage('test/test_cli/test_config_bad.yml')
+    return fromdir(__file__)('test_config_bad.yml')
 
 
-def test_postOptionsOk(self):
+def test_postOptionsOk(options, configGood):
     """
     Does a good config pass?
     """
-    pCONFIG_SCHEMA_YML = patch.object(aorta, 'CONFIG_SCHEMA_YML', self.configSchema)
+    options['configFile'] = configGood
     pOut = patch.object(sys, 'stdout', autospec=True)
     pErr = patch.object(sys, 'stderr', autospec=True)
-    with pCONFIG_SCHEMA_YML, pOut, pErr:
-        x = self.opt.postOptions()
-        self.assertEqual(True, x)
+    with pOut, pErr:
+        x = options.postOptions()
+        assert x == True
 
-def test_postOptionsBad(self):
+
+def test_postOptionsBad(options, configBad):
     """
     Does a bad config fail?
     """
-    pCONFIG_SCHEMA_YML = patch.object(aorta, 'CONFIG_SCHEMA_YML', self.configSchema)
     pOut = patch.object(sys, 'stdout', autospec=True)
     pErr = patch.object(sys, 'stderr', autospec=True)
-    self.opt['configFile'] = self.configBad
-
-    with pCONFIG_SCHEMA_YML, pOut, pErr:
-        self.assertRaises(ValidationError, self.opt.postOptions)
-
-def test_postOptionsSchema(self):
-    """
-    Can we display a normalized yaml output?
-    """
-    pOut = patch.object(sys, 'stdout', autospec=True)
-    pErr = patch.object(sys, 'stderr', autospec=True)
-    self.opt['schema'] = True
-    self.opt['configFile'] = self.configSchema
+    options['configFile'] = configBad
 
     with pOut, pErr:
-        x = self.opt.postOptions()
-        self.assertRegexpMatches(x, r'debug: {type: boolean}')
+        raises(ValidationError, options.postOptions)
 
-def test_parseArgs(self):
+
+def test_parseArgs(options):
     """
     Do we check permissions on files and report those errors?
     """
-    with patch.object(os, 'access', Mock(return_value=False)):
-        self.assertRaises(OSError, self.opt.parseArgs, 'adafds')
-    with patch.object(os, 'access', Mock(return_value=True)):
-        self.opt.parseArgs('cheeses')
-        self.assertEqual(self.opt['configFile'], 'cheeses')
+    with patch.object(os, 'access', return_value=False):
+        raises(OSError, options.parseArgs, 'adafds', 'fdsa')
+
+    with patch.object(os, 'access', side_effect=[True, False]):
+        raises(OSError, options.parseArgs, 'adafds', 'fdsa')
+
+    with patch.object(os, 'access', return_value=True):
+        options.parseArgs('cheeses', 'meats')
+        assert options['configFile'] == 'cheeses'
+        assert options['configSchema'] == 'meats'
 
