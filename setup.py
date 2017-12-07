c8ecab29793b6bf05e31387734c15c948be70d50
Ashley Fisher <fish.ash@gmail.com>
2017-12-07 13:57:55 -0800

    updates to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 from inspect import cleandoc
 
-from yamlschema._version import __version__
+
+_version = {}
+execfile('yamlschema/_version.py', _version)
 
 
 setup(
   name = 'yamlschema',
-  packages = ['yamlschema'],
-  version = __version__,
+  packages = ['yamlschema', 'yamlschema.test'],
+  version = _version['__version__'],
   description = 'A schema validator for YAML files',
   author = 'Ashley Fisher',
   author_email = 'fish.ash@gmail.com',
@@ -15,7 +17,8 @@ setup(
   keywords = ['yaml', 'schema'],
   classifiers = [],
   scripts = ['bin/yamlschema'],
-     install_requires=cleandoc('''
-        jsonschema==2.6.0
-        ''').split()
+  install_requires=cleandoc('''
+    codado>=0.4.997,<0.6
+    jsonschema==2.6.0
+    ''').split()
 )
